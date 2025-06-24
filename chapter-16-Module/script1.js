61422064e9305347d85666eaedb949a3f9794a3b
Uzair-Khane <uzairkhanar123@gmail.com>
2025-06-24 03:39:32 +0100

    add chapter 16 javascript module

---
+++ chapter-16-Module/script1.js
@@ -0,0 +1,3 @@
+export const fulname = function () {
+  return "uzair khan";  
+};
---
+++ chapter-16-Module/script2.js
@@ -0,0 +1,3 @@
+import { fulname } from "../chapter-16-Module/script1.js";
+
+document.getElementById("hi").innerHTML = fulname();
